3bea0be5efdcc7354f340f2e3cfc380313be2c67
Arison13 <arisonarias@gmail.com>
2021-12-16 16:25:57 -0500

    skeleton built and server up

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+// const Users = require('./users/users-model.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+
+module.exports = server;
